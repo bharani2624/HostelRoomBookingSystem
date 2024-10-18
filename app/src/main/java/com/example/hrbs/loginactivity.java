80d0735df714658b94dc23ceaf53ac0e1d5589b7
bharani2624 <bharani2624baru@gmail.com>
2024-10-18 19:04:51 +0530

    Partially Written Backend Logic For Login And Signup Pages

--- app/src/main/java/com/example/hrbs/loginactivity.java
+++ app/src/main/java/com/example/hrbs/loginactivity.java
@@ -1,24 +1,94 @@
 package com.example.hrbs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
 public class loginactivity extends AppCompatActivity {
 
+
+    EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loginactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_password);
+    }
+    public Boolean validateEmail()
+    {
+        String emailverify=email.getText().toString();
+        if(emailverify.isEmpty()) {
+            email.setError("Email Field Cannot Be Empty");
+            return false;
+        }
+        else {
+            email.setError(null);
+            return true;
+        }
+    }
+    public Boolean validatePassword()
+    {
+        String passwordverify=password.getText().toString();
+        if(passwordverify.isEmpty()) {
+            password.setError("Password Field Cannot Be Empty");
+            return false;
+        }
+        else {
+            password.setError(null);
+            return true;
+        }
+    }
+    public void checkUser()
+    {
+        String userEmail=email.getText().toString();
+        String userPassword=password.getText().toString();
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase=reference.orderByChild("email").equalTo(userEmail);
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    email.setError(null);
+                    String passwordDB=snapshot.child(userEmail).child("password").getValue(String.class);
+                    if(!Objects.equals(passwordDB,userPassword))
+                    {
+                        email.setError(null);
+                        Intent intent=new Intent(loginactivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        password.setError("Ivalid Password");
+                        password.requestFocus();
+                    }
+                }
+                else{email.setError("Invalid Email");}
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
+
     }
 }
--- app/src/main/java/com/example/hrbs/signupupactivity.java
+++ app/src/main/java/com/example/hrbs/signupupactivity.java
@@ -1,6 +1,12 @@
 package com.example.hrbs;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class signupupactivity extends AppCompatActivity {
 
+    EditText signupName,signupEmail,signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;//Since We are realtime database
+    DatabaseReference reference;//for reference
+    @SuppressLint("MissingInflatedId")//Incase If the id is not found
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signupupactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        signupName=findViewById(R.id.signup_name);
+        signupEmail=findViewById(R.id.signup_email);
+        signupPassword=findViewById(R.id.signup_password);
+        loginRedirectText=findViewById(R.id.redirectText);
+        signupButton=findViewById(R.id.signupbtn);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database=FirebaseDatabase.getInstance();
+                reference=database.getReference("users");
+                String name=signupName.getText().toString();
+                String email=signupEmail.getText().toString();
+                String password=signupPassword.getText().toString();
+                HelperClass helperClass=new HelperClass(name,email,password);
+                reference.child(name).setValue(helperClass);
+                Intent intent=new Intent(signupupactivity.this,loginactivity.class);
+                startActivity(intent);
+            }
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signupupactivity.this,loginactivity.class);
+                startActivity(intent);
+            }
         });
     }
 }

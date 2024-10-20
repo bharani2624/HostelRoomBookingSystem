7dcc774827087d8a2f7ce15dd536108d194a5747
bharani2624 <bharani2624baru@gmail.com>
2024-10-20 15:08:49 +0530

    Added Transition From Sapphire To Main Activity

--- app/src/main/java/com/example/hrbs/MainActivity.java
+++ app/src/main/java/com/example/hrbs/MainActivity.java
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.transition.Scene;
 import android.transition.TransitionManager;
+import android.transition.Slide;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,8 +21,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     CardView sapphire,emerald,pearl,coral,ruby,diamond;
-    private Scene sceneOne,sceneTwo;
-    private ViewGroup sceneRoot;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         coral=findViewById(R.id.coralCard);
         ruby=findViewById(R.id.rubyCard);
         diamond=findViewById(R.id.diamondCard);
+
         sapphire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/hrbs/sapphire.java
+++ app/src/main/java/com/example/hrbs/sapphire.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -28,6 +27,7 @@ public class sapphire extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(sapphire.this,MainActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
             }
         });
     }

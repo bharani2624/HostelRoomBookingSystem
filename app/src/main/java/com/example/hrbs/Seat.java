0abad5cb577f6b2f28f4f8cd9aae14ef5ea112eb
bharani2624 <bharani2624baru@gmail.com>
2024-10-21 14:27:16 +0530

    Made Some Changes

---
+++ app/src/main/java/com/example/hrbs/Seat.java
@@ -0,0 +1,4 @@
+package com.example.hrbs;
+
+public class Seat {
+}
--- app/src/main/java/com/example/hrbs/booksapphire.java
+++ app/src/main/java/com/example/hrbs/booksapphire.java
@@ -9,16 +9,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class booksapphire extends AppCompatActivity {
-
+    private booksapphire binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_booksapphire);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
